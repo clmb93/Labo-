b19416a470f1e5dc7e4c8aebdd438f68fbf1c223
LADISA Julien <j.ladisa@Accedia.local>
2017-10-04 13:44:25 +0200

    creation

--- Scene_complexe/complexe/js/script.js
+++ Scene_complexe/complexe/js/script.js
@@ -4,45 +4,27 @@ var camera = new THREE.PerspectiveCamera(75,window.innerWidth/window.innerHeight
 var renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth,window.innerHeight); //Ici la taille et largeur de la fenetre
 document.body.appendChild(renderer.domElement); //Crée un nouvel Element dans le dom , c'est le rendu
-camera.position.x = 25;  //On change la psoition de la camera sur l'axe x 
-camera.position.y = 25;  //On change la psoition de la camera sur l'axe y 
-camera.position.z = 15;  //On change la psoition de la camera sur l'axe z 
+camera.position.x = 5;  //On change la psoition de la camera sur l'axe x 
+camera.position.y = 5;  //On change la psoition de la camera sur l'axe y 
+camera.position.z = 5;  //On change la psoition de la camera sur l'axe z 
 camera.lookAt(scene.position);  // On indique à la camera de regarder la scene à la position ou elle se trouve 
 
 /* CREATION DES OBJETS */
 
 
-
-    var plato = new THREE.PlaneGeometry(30,40);
-    var materiel_plato = new THREE.MeshBasicMaterial({map : new THREE.TextureLoader().load('img/sol.jpg')});
-    var sol = new THREE.Mesh(plato,materiel_plato); // Création de l'objet sphere avec deux param , la forme et le materiel
-    sol.rotation.x = -Math.PI/2;
-    sol.position.set(0,0,0); //On définit la position de la sphere (x,z,y)
-    scene.add(sol);// Ajout de la sphere à la scene
+ var triangle_form = new THREE.Geometry();
+ 
+ triangle_form.vertices.push(
     
-    var mur_gauche = new THREE.PlaneGeometry(30,10);
-    var mur = new THREE.MeshBasicMaterial({map : new THREE.TextureLoader().load('img/mur.jpg')});
-    var murG = new THREE.Mesh(mur_gauche,mur)
-    sol.add(murG);
-    murG.rotation.x=Math.PI/2;
-    murG.rotation.y=Math.PI;
-    murG.position.y=-20; 
-    murG.position.z=5; 
-    
-    
-    var murD = new THREE.Mesh(mur_gauche,mur)
-    sol.add(murD);
-    murD.rotation.x=-Math.PI/2;
-    murD.rotation.y=-Math.PI;
-    murD.position.y=20; 
-    murD.position.z=5; 
-    
-    var murFond  = new THREE.PlaneGeometry(10,40);
-    var murF = new THREE.Mesh(murFond,mur)
-    sol.add(murF);
-    murF.rotation.y=Math.PI/2;
-    murF.position.z=5; 
-    murF.position.x=-15;
+    new THREE.Vector3(0,0,1),
+    new THREE.Vector3(0,1,0),
+    new THREE.Vector3(1,0,0)
+ );
+ 
+ triangle_form.faces.push(new THREE.Face3(0,1,2,3));
+ 
+ var tr = new THREE.Mesh(triangle_form,new THREE.MeshBasicMaterial({color : 0xff0000, wireframe:true}));
+  scene.add(tr);
 
     
 /*CREATION DE LA LUMIERE AMBIANTE */
